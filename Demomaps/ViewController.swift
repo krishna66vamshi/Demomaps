1b73912a005d0afe556653e6154be33ab9ff1b05
vamsi <krishna66vamshi@gmail.com>
2019-04-06 17:27:35 +0530

    Full code.

--- Demomaps/ViewController.swift
+++ Demomaps/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GMSServices.provideAPIKey("AIzaSyAnfypX6EWEjJndRRF-hR4z2kcOU-ms1xM")
+        let camera =  GMSCameraPosition.camera(withLatitude: 30.707905, longitude: 76.712149, zoom: 10)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.title = "Title"
+        marker.snippet = "Subtitle"
+        marker.position = CLLocationCoordinate2D(latitude: 30.707905, longitude: 76.712149)
+        marker.map = mapView
+        
     }
-
-
+    
+    
 }
 
---
+++ Pods/Target Support Files/Pods-Demomaps/Pods-Demomaps-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Demomaps : NSObject
+@end
+@implementation PodsDummy_Pods_Demomaps
+@end
